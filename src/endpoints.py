263e17bd6dc2acc8bddb48edf65e19f060798578
hillo <104848741+Betaliza@users.noreply.github.com>
2024-11-01 14:47:09 +0300

    Update endpoints.py

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,1 +1,17 @@
+github_repo_latest = "https://github.com/Betaliza/Spoofer/releases/"
+asset_delivery = "https://assetdelivery.roblox.com/v1/asset/?id="
+asset_info = "https://develop.roblox.com/v1/assets?assetIds="
+authenticated = "https://users.roblox.com/v1/users/authenticated"
 
+def getPublishUrl(assetTypeName, name, creatorId, isGroup):
+    return (
+        "https://www.roblox.com/ide/publish/uploadnewanimation?"
+        f"assetTypeName={ assetTypeName }"
+        f"&name={ name }"
+        "&description=hillo🤑🤑"
+        "&AllID=1"
+        "&ispublic=False"
+        "&allowComments=True"
+        "&isGamesAsset=False"
+        f"{ '&groupId=' + str(creatorId) if isGroup else '' }"
+    )
